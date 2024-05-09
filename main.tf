9f495bd836e87db6fcc403bfce8094b6195e4f29
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-09 17:47:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,119 +1,69 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 # Configure and downloading plugins for aws
 provider "aws" {
   region     = "${var.aws_region}"
 }
 
-# Creating VPC
-resource "aws_vpc" "demovpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "default"
+resource "random_pet" "sg" {}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
   tags = {
-    Name = "Demo VPC"
+    Name = "HelloWorld_4"
   }
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id = "subnet-03274bc85f950b9ed"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
 }
 
-# Creating Internet Gateway 
-resource "aws_internet_gateway" "demogateway" {
-  vpc_id = "${aws_vpc.demovpc.id}"
-}
-
-# Grant the internet access to VPC by updating its main route table
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.demovpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.demogateway.id}"
-}
-
-# Creating 1st subnet 
-resource "aws_subnet" "demosubnet" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "Demo subnet"
-  }
-}
-
-# Creating 2nd subnet 
-resource "aws_subnet" "demosubnet1" {
-  vpc_id                  = "${aws_vpc.demovpc.id}"
-  cidr_block             = "${var.subnet1_cidr}"
-  map_public_ip_on_launch = true
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Demo subnet 1"
-  }
-}
-
-# Creating Security Group
-resource "aws_security_group" "demosg" {
-  name        = "Demo Security Group"
-  description = "Demo Module"
-  vpc_id      = "${aws_vpc.demovpc.id}"
-
-  # Inbound Rules
-  # HTTP access from anywhere
+resource "aws_security_group" "web-sg" {
+  vpc_id = "vpc-08d2e887c6294169a"
+  name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Splunk default port
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  # Replication Port
-  ingress {
-    from_port   = 8089
-    to_port     = 8089
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Management Port
-  ingress {
-    from_port   = 4598
-    to_port     = 4598
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Ingestion Port
-  ingress {
-    from_port   = 9997
-    to_port     = 9997
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound Rules
-  # Internet access to anywhere
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
     to_port     = 0
@@ -122,77 +72,6 @@ resource "aws_security_group" "demosg" {
   }
 }
 
-# Creating key pair
-resource "aws_key_pair" "demokey" {
-  key_name   = "${var.key_name}"
-  public_key = "${file(var.public_key)}"
-}
-  
-# Creating EC2 Instance
-resource "aws_instance" "demoinstance" {
-
-  # AMI based on region 
-  ami = "${lookup(var.ami, var.aws_region)}"
-
-  # Launching instance into subnet 
-  subnet_id = "${aws_subnet.demosubnet.id}"
-
-  # Instance type 
-  instance_type = "${var.instancetype}"
-  
-  # Count of instance
-  count= "${var.master_count}"
-  
-  # SSH key that we have generated above for connection
-  key_name = "${aws_key_pair.demokey.id}"
-
-  # Attaching security group to our instance
-  vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
-
-  # Attaching Tag to Instance 
-  tags = {
-    Name = "Search-Head-${count.index + 1}"
-  }
-  
-  # Root Block Storage
-  root_block_device {
-    volume_size = "40"
-    volume_type = "standard"
-  }
-  
-  #EBS Block Storage
-  ebs_block_device {
-    device_name = "/dev/sdb"
-    volume_size = "80"
-    volume_type = "standard"
-    delete_on_termination = false
-  }
-  
-  # SSH into instance 
-  connection {
-    
-    # Host name
-    host = self.public_ip
-    # The default username for our AMI
-    user = "ec2-user"
-    # Private key for connection
-    private_key = "${file(var.private_key)}"
-    # Type of connection
-    type = "ssh"
-  }
-  
-  # Installing splunk on newly created instance
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker -y",
-      "sudo service docker start",
-      "sudo usermod -a -G docker ec2-user",
-      "sudo chkconfig docker on",
-      "sudo yum install -y git",
-      "sudo chmod 666 /var/run/docker.sock",
-      "docker pull dhruvin30/dhsoniweb:v1",
-      "docker run -d -p 80:80 dhruvin30/dhsoniweb:latest"   
-  ]
- }
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
