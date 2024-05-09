9444258e690494b163ef7db6ae8ba9d60eb1a3ee
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-09 18:05:47 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,19 @@
-# Defining Region
 variable "aws_region" {
   default = "us-east-2"
+}
+
+variable "vpc_id" {
+  default = "vpc-08d2e887c6294169a"
+}
+
+variable "subnet_id" {
+  default = "subnet-03274bc85f950b9ed"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_tag_name" {
+  default = "HelloWorld_5"
 }
