e52a12ef4a66f53251c80db1d1313a36f97ebdc1
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-10 16:11:25 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -17,3 +17,19 @@ variable "instance_type" {
 variable "instance_tag_name" {
   default = "HelloWorld_5"
 }
+
+# These resources must already exist. I created them here: https://github.com/mmwillingham/github_actions-terraform-aws-backend/blob/main/vars.tf
+
+variable "s3_bucket_name" {
+  default = "bolauder-tfstate"
+}
+
+variable "s3_key" {
+  default = "terraform.tfstate"
+}
+
+variable "dynamoDB_table_name" {
+  default = "bolauder-tfstate"
+}
+
+
