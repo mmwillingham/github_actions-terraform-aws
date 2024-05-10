9de7236f3b84262403fb210dbfbf8f8ce53f0c32
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2024-05-10 16:05:27 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -3,11 +3,11 @@ variable "aws_region" {
 }
 
 variable "vpc_id" {
-  default = "vpc-08d2e887c6294169a"
+  default = "vpc-0b6188cf85ce4e65f"
 }
 
 variable "subnet_id" {
-  default = "subnet-03274bc85f950b9ed"
+  default = "subnet-0d31b57f33474bf4e"
 }
 
 variable "instance_type" {
